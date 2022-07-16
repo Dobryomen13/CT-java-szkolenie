4ec1d5eaea5a9a00d381e8d2b7f01e8b37152261
Karolina <karolina.k.karas@gmail.com>
2022-07-16 08:40:52 +0200

    dodanie klasy petla, wyswietlanie liczb 0 - 100

--- src/main/java/pl/cyber/trainees/SimpleApp.java
+++ src/main/java/pl/cyber/trainees/SimpleApp.java
@@ -2,6 +2,10 @@ package pl.cyber.trainees;
 
 import pl.cyber.trainees.dziedziczenie.Kolor;
 import pl.cyber.trainees.dziedziczenie.Model;
+import pl.cyber.trainees.dziedziczenie.Pojazd;
+
+import java.util.ArrayList;
+import java.util.List;
 
 
 public class SimpleApp {
@@ -10,7 +14,7 @@ public class SimpleApp {
         var modelPojazdu2 = new Model("VW", "2020",
         "przod",  "Passat",  1000, 2.0, Kolor.GREEN);
 
-        modelPojazdu1.setKolor(Kolor.RED );
+        modelPojazdu1.setKolor(Kolor.RED);
         modelPojazdu1.setMarka("Ford");
         modelPojazdu1.setNaped("4x4");
         modelPojazdu1.setRocznik("1967");
@@ -18,11 +22,41 @@ public class SimpleApp {
         modelPojazdu1.setPojemonscSilnika(2.0);
         modelPojazdu1.setPrzebieg(0);
 
-
-
      //   System.out.println(modelPojazdu2.toString());
             System.out.println(modelPojazdu1);
 
+        List<String> strings = new ArrayList<>();
+        strings.add("raz");
+        strings.add("dwa");
+
+        System.out.println(strings);
+
+
+     // kolekcje w java to zbiory danych
+     /* 4 rodzaje kolekcji:
+         1. List - list - gwarantuje kolejnosc
+         2. Set - zbior - nie gwarantuje kolejnosci
+         3. Map - mapa - przechowuje wartosc jako klucz
+         4. Queue - kolejka - podobnie do listy, ale mozemy stosowac
+         algorytmy LIFO i FIFO
+         LIFO - Last In First Out
+         FIFO - First In First Out
+
+         Najpopularniejsze implementacja:
+         List:
+         ArrayList() - talica listy, ktora nie przechowuje info o poprzednim i nastepnym rekordzie
+         LinkedList() -  talica listy, ktora przechowuje info o poprzednim i nastepnym rekordzie
+
+         String nowyString = "";
+         List<Model> modele = new ArrayList<>(); w <> przechowujemy kokretny typdanych
+         modele.add(modelPojazdu1);
+
+         List<String> lista = new ArrayList<>();
+
+         List<String> lista = new ArrayList<String
+
+
+       */
     }
 
 }
---
+++ src/main/java/spotkania/Petle.java
@@ -0,0 +1,23 @@
+package spotkania;
+
+public class Petle {
+
+    public static void main(String[] args) {
+        int liczba = 100;
+
+        while (liczba > 0) {
+            liczba--;
+            if (liczba < 100) {
+            }
+        }
+            for(int i = 0;i <= 100; i++)
+            {
+                if(i == 0)
+                {
+                    System.out.println();
+                }
+                System.out.print(i + " ");
+          //  System.out.print(liczba + liczba);
+        }
+    }
+}
