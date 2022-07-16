4ed77091fd73d362dc6e145c4e8dfff596b432aa
Karolina <karolina.k.karas@gmail.com>
2022-07-16 08:56:34 +0200

    dodanie klasy petla, wyswietlanie liczb 0 - 100. Dodanie przecinkow i kropki

--- src/main/java/spotkania/Petle.java
+++ src/main/java/spotkania/Petle.java
@@ -7,17 +7,15 @@ public class Petle {
 
         while (liczba > 0) {
             liczba--;
-            if (liczba < 100) {
-            }
+            if (liczba < 100) ;
         }
-            for(int i = 0;i <= 100; i++)
-            {
-                if(i == 0)
-                {
-                    System.out.println();
-                }
-                System.out.print(i + " ");
-          //  System.out.print(liczba + liczba);
+        for (int i = 0; i <= 100; i++) {
+            if (i < 100) {
+                System.out.print(i + ", ");
+            }
+            if (i == 100) {
+                System.out.println(i+ ".");
+            }
         }
     }
 }
